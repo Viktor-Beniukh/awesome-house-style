6830651d522726a635302870840bec4dfb4ffaff
Viktor Beniukh <superbigbenjamin@gmail.com>
2023-12-23 20:48:36 +0200

    Added Category & Product models

--- goods/admin.py
+++ goods/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
-# Register your models here.
+from goods.models import Category, Product
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "slug",)
+    prepopulated_fields = {"slug": ("name",)}
+    search_fields = ("name",)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "slug",
+        "image_show",
+        "price",
+        "quantity",
+        "created_at",
+        "updated_at"
+    )
+    list_filter = ("created_at", "updated_at")
+    list_editable = ("price",)
+    prepopulated_fields = {"slug": ("name",)}
+    search_fields = ("name",)
+
+    def image_show(self, obj):
+        if obj.image:
+            return mark_safe(
+                "<img src='{}' width='60' />".format(obj.image.url)
+            )
+        return "None"
+
+    image_show.__name__ = "Picture"
--- goods/models.py
+++ goods/models.py
@@ -1,3 +1,50 @@
-from django.db import models
+import os
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.utils.text import slugify
+
+
+def product_image_file_path(instance, filename):
+    _, extension = os.path.splitext(filename)
+    filename = f"{slugify(instance.name)}-{uuid.uuid4()}{extension}"
+
+    return os.path.join("uploads/products/", filename)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, unique=True, db_index=True)
+    slug = models.SlugField(max_length=255, unique=True, db_index=True)
+
+    class Meta:
+        ordering = ("name",)
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+        index_together = (("id", "slug"),)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255, db_index=True)
+    slug = models.SlugField(max_length=255, db_index=True, unique=True)
+    image_product = models.ImageField(
+        upload_to=product_image_file_path, blank=True, null=True
+    )
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    discount = models.DecimalField(max_digits=5, decimal_places=2, default=0.00)
+    quantity = models.PositiveIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(
+        to=Category, on_delete=models.CASCADE, related_name="products"
+    )
+
+    class Meta:
+        ordering = ("name",)
+        index_together = (("id", "slug"),)
+
+    def __str__(self):
+        return self.name
