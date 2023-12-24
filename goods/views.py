8063fe82d0ca446d84a86c5397cfec31f108c94d
Viktor Beniukh <superbigbenjamin@gmail.com>
2023-12-24 10:29:56 +0200

    Added goods data to context of catalog view in goods views.py

--- goods/views.py
+++ goods/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 
+from goods.models import Product
+
 
 def catalog(request):
+    goods = Product.objects.all()
+
     context = {
         "title": "House Style - Catalog",
+        "goods": goods
     }
     return render(request=request, template_name="goods/catalog.html", context=context)
 
