fa40db41aa688b74547e7249f4659c5e87d0d0dd
Viktor Beniukh <superbigbenjamin@gmail.com>
2023-12-24 19:36:44 +0200

    Commented postgres library in utils.py

--- goods/utils.py
+++ goods/utils.py
@@ -1,10 +1,10 @@
 from django.db.models import Q
-from django.contrib.postgres.search import (
-    SearchVector,
-    SearchQuery,
-    SearchRank,
-    SearchHeadline,
-)
+# from django.contrib.postgres.search import (
+#     SearchVector,
+#     SearchQuery,
+#     SearchRank,
+#     SearchHeadline,
+# )
 
 from goods.models import Product
 
