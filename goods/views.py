24b45f8a1897eefe34ffa37ab7c56e2566369a1d
Viktor Beniukh <superbigbenjamin@gmail.com>
2023-12-24 18:01:21 +0200

    Fixed filter to catalog.html

--- goods/views.py
+++ goods/views.py
@@ -2,15 +2,28 @@ from django.core.paginator import Paginator
 from django.shortcuts import render, get_list_or_404
 
 from goods.models import Product
+from goods.utils import q_search
 
 
-def catalog(request, category_slug: str = None):
+def catalog_view(request, category_slug: str = None):
+    page = request.GET.get("page", 1)
+    on_sale = request.GET.get("on_sale", None)
+    order_by = request.GET.get("order_by", None)
+    query = request.GET.get("q", None)
+
     if category_slug == "all-goods":
         goods = Product.objects.all()
+    elif query:
+        goods = q_search(query)
     else:
         goods = get_list_or_404(Product.objects.filter(category__slug=category_slug))
 
-    page = request.GET.get("page", 1)
+    if on_sale:
+        goods = goods.filter(discount__gt=0)
+
+    if order_by and order_by != "default":
+        goods = goods.order_by(order_by)
+
     paginator = Paginator(goods, 3)
     current_page = paginator.page(int(page))
 
