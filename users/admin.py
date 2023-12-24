8214e22eac194fbb54ab8b83d550be976a27db8c
Viktor Beniukh <superbigbenjamin@gmail.com>
2023-12-24 22:26:34 +0200

    Added image and phone fields to user model

--- users/admin.py
+++ users/admin.py
@@ -3,4 +3,23 @@ from django.contrib.auth.admin import UserAdmin
 
 from users.models import User
 
-admin.site.register(User, UserAdmin)
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("phone_number", "image_user")
+    fieldsets = UserAdmin.fieldsets + (
+        (("Additional info", {"fields": ("phone_number", "image_user")}),)
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            (
+                "Additional info",
+                {
+                    "fields": (
+                        "image_user",
+                        "phone_number",
+                    )
+                },
+            ),
+        )
+    )
--- users/models.py
+++ users/models.py
@@ -1,5 +1,39 @@
+import os
+import uuid
+
+from PIL import Image
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.text import slugify
+from phonenumber_field.modelfields import PhoneNumberField
+
+
+def user_image_file_path(instance, filename):
+    _, extension = os.path.splitext(filename)
+    filename = f"{slugify(instance.username)}-{uuid.uuid4()}{extension}"
+
+    return os.path.join("uploads/user_pics/", filename)
 
 
 class User(AbstractUser):
-    pass
+    image_user = models.ImageField(upload_to=user_image_file_path, blank=True, null=True)
+    phone_number = PhoneNumberField(blank=True, null=True)
+
+    def __str__(self):
+        return self.username
+
+    def save(
+        self,
+        force_insert=False,
+        force_update=False,
+        using=None,
+        update_fields=None
+    ):
+        super().save()
+
+        img = Image.open(self.image_user.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image_user.path)
