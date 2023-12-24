02ede430c416bc8cc0ef76721c8124ec55dffc2a
Viktor Beniukh <superbigbenjamin@gmail.com>
2023-12-24 16:18:44 +0200

    Added pagination

--- goods/views.py
+++ goods/views.py
@@ -1,17 +1,23 @@
-from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
+from django.shortcuts import render, get_list_or_404
 
 from goods.models import Product
 
 
-def catalog(request, category_slug: str):
+def catalog(request, category_slug: str = None):
     if category_slug == "all-goods":
         goods = Product.objects.all()
     else:
-        goods = get_object_or_404(Product.objects.filter(category__slug=category_slug))
+        goods = get_list_or_404(Product.objects.filter(category__slug=category_slug))
+
+    page = request.GET.get("page", 1)
+    paginator = Paginator(goods, 3)
+    current_page = paginator.page(int(page))
 
     context = {
         "title": "House Style - Catalog",
-        "goods": goods
+        "goods": current_page,
+        "slug_url": category_slug
     }
 
     return render(
