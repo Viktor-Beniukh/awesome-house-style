5aab5a2552727ff3c194b8567d3cbe21262b64bf
Viktor Beniukh <superbigbenjamin@gmail.com>
2023-12-24 18:53:45 +0200

    Added search of products by id, name and description (for SQLite3)

--- goods/urls.py
+++ goods/urls.py
@@ -5,6 +5,7 @@ from goods import views
 app_name = "goods"
 
 urlpatterns = [
+    path("search/", views.catalog_view, name="search"),
     path("<slug:category_slug>/", views.catalog_view, name="index"),
     path("product/<slug:product_slug>/", views.product_view, name="product"),
 ]
