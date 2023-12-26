a1a3df55818dd26e722ee8bd648d8e6e11ab91ad
Viktor Beniukh <superbigbenjamin@gmail.com>
2023-12-26 21:12:09 +0200

    Added cart templatetags with tag and cart add view

--- carts/views.py
+++ carts/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from carts.models import Cart
+from goods.models import Product
 
 
 def cart_add_view(request, product_slug: str):
-    pass
+    product = Product.objects.get(slug=product_slug)
+
+    if request.user.is_authenticated:
+        carts = Cart.objects.filter(user=request.user, product=product)
+
+        if carts.exists():
+            cart = carts.first()
+            if cart:
+                cart.quantity += 1
+                cart.save()
+        else:
+            Cart.objects.create(user=request.user, product=product, quantity=1)
+
+    return redirect(request.META["HTTP_REFERER"])
 
 
 def cart_change_view(request, product_slug: str):
