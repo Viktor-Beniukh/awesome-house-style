17f58c1a91a4d9c14bbd8933fa147bb5a01c6120
Viktor Beniukh <superbigbenjamin@gmail.com>
2023-12-26 16:22:51 +0200

    Added login, register, logout options and user profile to users object

---
+++ users/forms.py
@@ -0,0 +1,51 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UserChangeForm
+
+from users.models import User
+
+
+class UserRegisterForm(UserCreationForm):
+    first_name = forms.CharField(required=True)
+    last_name = forms.CharField(required=True)
+    username = forms.CharField()
+    email = forms.EmailField()
+    password1 = forms.CharField()
+    password2 = forms.CharField()
+
+    class Meta:
+        model = User
+        fields = (
+            "first_name",
+            "last_name",
+            "username",
+            "email",
+            "password1",
+            "password2",
+        )
+
+
+class UserLoginForm(AuthenticationForm):
+    username = forms.CharField()
+    password = forms.CharField()
+
+    class Meta:
+        model = User
+        fields = ("username", "password")
+
+
+class UserProfileForm(UserChangeForm):
+    image_user = forms.ImageField(required=False)
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    username = forms.CharField()
+    email = forms.CharField()
+
+    class Meta:
+        model = User
+        fields = (
+            "image_user",
+            "first_name",
+            "last_name",
+            "username",
+            "email",
+        )
--- users/views.py
+++ users/views.py
@@ -1,3 +1,75 @@
-from django.shortcuts import render
+from django.contrib import auth, messages
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.urls import reverse
 
-# Create your views here.
+from users.forms import UserLoginForm, UserProfileForm, UserRegisterForm
+
+
+def login_view(request):
+    if request.method == "POST":
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST["username"]
+            password = request.POST["password"]
+            user = auth.authenticate(username=username, password=password)
+
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse("main:index"))
+    else:
+        form = UserLoginForm()
+
+    context = {
+        "title": "House Style - Authorization",
+        "form": form
+    }
+
+    return render(request=request, template_name="users/login.html", context=context)
+
+
+def register_view(request):
+    if request.method == "POST":
+        form = UserRegisterForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+
+            user = form.instance
+            auth.login(request, user)
+
+            messages.success(request, f"{user.username}, You have successfully registered and logged in")
+            return HttpResponseRedirect(reverse("main:index"))
+    else:
+        form = UserRegisterForm()
+
+    context = {
+        "title": "House Style - Registration",
+        "form": form
+    }
+    return render(request=request, template_name="users/register.html", context=context)
+
+
+@login_required
+def profile_view(request):
+    if request.method == "POST":
+        form = UserProfileForm(data=request.POST, instance=request.user, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Profile successfully updated")
+            return HttpResponseRedirect(reverse("user:profile"))
+    else:
+        form = UserProfileForm(instance=request.user)
+
+    context = {
+        "title": "House Style - Personal Account",
+        "form": form,
+    }
+    return render(request=request, template_name="users/profile.html", context=context)
+
+
+@login_required
+def logout_view(request):
+    messages.success(request, f"{request.user.username}, You left your account")
+    auth.logout(request)
+    return redirect(reverse("main:index"))
