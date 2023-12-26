aafdd5659270dc9598f621a23c86de4c1fab481f
Viktor Beniukh <superbigbenjamin@gmail.com>
2023-12-26 18:50:03 +0200

    Added users cart data

--- users/urls.py
+++ users/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path("login/", views.login_view, name="login"),
     path("register/", views.register_view, name="register"),
     path("profile/", views.profile_view, name="profile"),
+    path("users-cart/", views.users_cart_view, name="users_cart"),
     path("logout/", views.logout_view, name="logout"),
 ]
