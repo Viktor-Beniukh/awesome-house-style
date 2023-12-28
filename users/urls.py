ed83b380581f679918d829d2ac4720424f479dba
Viktor Beniukh <superbigbenjamin@gmail.com>
2023-12-28 16:01:27 +0200

    Added templates to reset password

--- users/urls.py
+++ users/urls.py
@@ -12,4 +12,16 @@ urlpatterns = [
     path("logout/", views.logout_view, name="logout"),
     path("<int:pk>/password/", views.change_password_view, name="password-change"),
     path("password-success/", views.password_success_view, name="password-success"),
+    path(
+        "reset_password/", views.reset_password_view, name="reset_password"
+    ),
+    path(
+        "reset_password_sent/", views.reset_password_done_view, name="password_reset_done"
+    ),
+    path(
+        "reset/<uidb64>/<token>/", views.reset_password_confirm_view, name="password_reset_confirm"
+    ),
+    path(
+        "reset_password_complete/", views.reset_password_complete_view, name="password_reset_complete"
+    ),
 ]
