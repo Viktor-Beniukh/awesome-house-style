2c4ed48ba0b92a2177bc02c4b2604d941e90a8ed
Viktor Beniukh <superbigbenjamin@gmail.com>
2024-01-12 21:48:44 +0200

    Added possible to login by email

--- users/models.py
+++ users/models.py
@@ -2,9 +2,11 @@ import os
 import uuid
 
 from PIL import Image
+from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.text import slugify
+from django.utils.translation import gettext as _
 from phonenumber_field.modelfields import PhoneNumberField
 
 
@@ -15,9 +17,48 @@ def user_image_file_path(instance, filename):
     return os.path.join("uploads/user_pics/", filename)
 
 
+class UserManager(BaseUserManager):
+    """Define a model manager for User model."""
+
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        """Create and save a User with the given email and password."""
+        if not email:
+            raise ValueError("The given email must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create and save a regular User with the given email and password."""
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        """Create and save a SuperUser with the given email and password."""
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self._create_user(email, password, **extra_fields)
+
+
 class User(AbstractUser):
     image_user = models.ImageField(upload_to=user_image_file_path, blank=True, null=True)
     phone_number = PhoneNumberField(blank=True, null=True)
+    email = models.EmailField(_("email address"), unique=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+    objects = UserManager()
 
     def __str__(self):
         return self.username
