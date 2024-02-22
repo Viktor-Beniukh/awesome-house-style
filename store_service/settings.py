5df4d18958b5f946c69ac53ef7fc870fb8df665e
Viktor Beniukh <superbigbenjamin@gmail.com>
2024-02-22 18:17:27 +0200

    Added test db to settings.py

--- store_service/settings.py
+++ store_service/settings.py
@@ -115,6 +115,12 @@ else:
         }
     }
 
+if "test" in sys.argv:
+    DATABASES["default"] = {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db_test.sqlite3"
+    }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
